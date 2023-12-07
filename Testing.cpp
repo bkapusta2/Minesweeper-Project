fb6cec287d7e7f053a2865cbcf19cf9c1b1eb779
Brian Kapusta <bkapusta2@gmail.com>
2023-12-07 15:52:57 -0500

    Test suite creation

--- Testing.cpp
+++ Testing.cpp
@@ -7,82 +7,100 @@
 #include "lib/include/OpenCell.h"
 #include "lib/include/BombCell.h"
 
-TEST_CASE("Check for board size") {
-    Board board;
-    CHECK(board.getBoardSize() == 10);
+TEST_CASE("Testing board creation") {
+    SUBCASE("Check for board size") {
+        Board board;
+        CHECK(board.getBoardSize() == 10);
+    }
+
+    SUBCASE("Check for instantiated cell types") {
+        Board board;
+        board.setCells();
+        board.setCellValues();
+
+        CHECK(board.board[0][0]->getSymbol() != 'P');
+        CHECK(board.board[5][3]->getSymbol() != 'P');
+        CHECK(board.board[8][3]->getSymbol() != 'P');
+    }
 }
 
-TEST_CASE("Check for instantiated cell types") {
-    Board board;
-    board.setCells();
-    board.setCellValues();
+TEST_CASE("On click related tests") {
+    SUBCASE("Test for clicking") {
+        Board board;
+        board.setCells();
+        board.setCellValues();
 
-    CHECK(board.board[0][0]->getSymbol() != 'P');
-    CHECK(board.board[5][3]->getSymbol() != 'P');
-    CHECK(board.board[8][3]->getSymbol() != 'P');
-}
+        board.clickCell(3, 4);
+        CHECK(board.board[3][4]->isRevealed() == true);
+    }
 
-TEST_CASE("Test for clicking") {
-    Board board;
-    board.setCells();
-    board.setCellValues();
-
-    board.clickCell(3, 4);
-    CHECK(board.board[3][4]->isRevealed() == true);
-}
-
-// IMPLEMENT
-TEST_CASE("Test for game end on only bombs left IMPLEMENT") {
-    Board board;
-    board.setCells();
-    board.setCellValues();
-    for (int y = 0; y < board.getBoardSize(); y++) {
-        for (int x = 0; x < board.getBoardSize(); x++) {
-            if (board.board[x][y]->getSymbol() == 'O') {
-                board.clickCell(x, y);
+    SUBCASE("Test for game end on only bombs left IMPLEMENT") {
+        Board board;
+        board.setCells();
+        board.setCellValues();
+        for (int y = 0; y < board.getBoardSize(); y++) {
+            for (int x = 0; x < board.getBoardSize(); x++) {
+                if (board.board[x][y]->getSymbol() == 'O') {
+                    board.clickCell(x, y);
+                }
             }
         }
+        board.checkGameSuccess();
+        CHECK(board.gameEnded == true);
     }
-    board.checkGameSuccess();
-    CHECK(board.gameEnded == true);
+
+    SUBCASE("Test for game end on bomb click") {
+        Board board;
+        board.setCells();
+        board.board[0][0] = new BombCell();
+        board.setCellValues();
+
+        board.clickCell(0, 0);
+        CHECK(board.board[0][0]->isRevealed() == true);
+        CHECK(board.gameEnded == true);
+    }
 }
 
-TEST_CASE("Test for game end on bomb click") {
-    Board board;
-    board.setCells();
-    board.board[0][0] = new BombCell();
-    board.setCellValues();
+TEST_CASE("Cell value tests") {
+    SUBCASE("Checking for inner value bomb") {
+        Board board;
+        board.setCells();
+        board.board[0][0] = new BombCell();
+        board.setCellValues();
+        CHECK(board.board[0][0]->getValueInside() == -1);
+    }
 
-    board.clickCell(0, 0);
-    CHECK(board.board[0][0]->isRevealed() == true);
-    CHECK(board.gameEnded == true);
+    SUBCASE("Checking for inner value open cell") {
+        Board board;
+        board.setCells();
+        board.board[0][0] = new OpenCell();
+        board.setCellValues();
+        CHECK(board.board[0][0]->getValueInside() >= 0);
+    }
 }
 
 TEST_CASE("Testing board reset") {
-    Board board;
-    board.setCells();
-    board.setCellValues();
+    SUBCASE("Board reset prior to loss") {
+        Board board;
+        board.setCells();
+        board.setCellValues();
 
-    board.clickCell(6, 6);
-    CHECK(board.board[6][6]->isRevealed() == true);
-    board.resetBoard();
-    CHECK(board.board[6][6]->isRevealed() == false);
-}
+        board.clickCell(6, 6);
+        CHECK(board.board[6][6]->isRevealed() == true);
+        board.resetBoard();
+        CHECK(board.board[6][6]->isRevealed() == false);
+    }
 
-TEST_CASE("Checking for inner value bomb") {
-    Board board;
-    board.setCells();
-    board.board[0][0] = new BombCell();
-    board.setCellValues();
-    CHECK(board.board[0][0]->getValueInside() == -1);
-}
-
-TEST_CASE("Checking for inner value open cell") {
-    Board board;
-    board.setCells();
-    board.board[0][0] = new OpenCell();
-    board.setCellValues();
-    CHECK(board.board[0][0]->getValueInside() >= 0);
+    SUBCASE("Board reset after loss") {
+        Board board;
+        board.setCells();
+        board.board[1][1] = new BombCell();
+        board.setCellValues();
+        board.clickCell(1,1);
+        CHECK(board.gameEnded == true);
+        board.resetBoard();
+        CHECK(board.gameEnded == false);
+    }
 }
 
 TEST_CASE("Test revealing all bombs on loss") {
@@ -99,6 +117,7 @@ TEST_CASE("Test revealing all bombs on loss") {
     CHECK(board.board[4][4]->isRevealed() == true);
     CHECK(board.board[5][5]->isRevealed() == true);
 }
+
 
 
 
